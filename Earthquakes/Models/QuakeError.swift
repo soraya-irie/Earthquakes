31765834c315a9ded9954c0651065ede84991335
soraya-ire <rujiangkongshi@gmail.com>
2026-05-29 09:18:38 +0900

    Complete Section1 Step9 - decoding structured json

--- Earthquakes/Models/QuakeError.swift
+++ Earthquakes/Models/QuakeError.swift
@@ -3,3 +3,14 @@ import Foundation
 enum QuakeError: Error {
     case missingData
 }
+
+extension QuakeError: LocalizedError {
+    var errorDescription: String? {
+        switch self {
+        case .missingData:
+            return NSLocalizedString(
+                "Found and will discard a quake missing a valid code, magnitude, place, or time.",
+                comment: "")
+        }
+    }
+}
