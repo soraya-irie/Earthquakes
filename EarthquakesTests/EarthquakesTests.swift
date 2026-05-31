7598c010a9b04a11d894d5878da026cc6b5c1abd
soraya-ire <rujiangkongshi@gmail.com>
2026-05-31 11:53:07 +0900

    Complete Section2 Step6 - decoding structured json

--- EarthquakesTests/EarthquakesTests.swift
+++ EarthquakesTests/EarthquakesTests.swift
@@ -1,18 +1,7 @@
-//
-//  EarthquakesTests.swift
-//  EarthquakesTests
-//
-//  Created by soraya irie on 2026/05/31.
-//
-
+import Foundation
 import Testing
+@testable import Earthquakes
 
 struct EarthquakesTests {
-
-    @Test func example() async throws {
-        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
-        // Swift Testing Documentation
-        // https://developer.apple.com/documentation/testing
-    }
 
 }
