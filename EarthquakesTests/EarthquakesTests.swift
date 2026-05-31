e806fd893bc72835a95b07b680fc0558e38808d2
soraya-ire <rujiangkongshi@gmail.com>
2026-05-31 12:09:53 +0900

    Complete Section2 Step10 - decoding structured json

--- EarthquakesTests/EarthquakesTests.swift
+++ EarthquakesTests/EarthquakesTests.swift
@@ -8,5 +8,8 @@ struct EarthquakesTests {
         let decoder = JSONDecoder()
         let quake = try decoder.decode(Quake.self, from: testFeature_nc73649170)
         #expect(quake.code == "73649170")
+
+        let expectedSeconds = TimeInterval(1636129710550 / 1000.0)
+        let expectedTime = Date(timeIntervalSince1970: expectedSeconds)
     }
 }
