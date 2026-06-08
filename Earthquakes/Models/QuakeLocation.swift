6b14bc6ad946713ca08c4bed7456e945e80a81ae
soraya-ire <rujiangkongshi@gmail.com>
2026-06-08 11:02:19 +0900

    Complete Section4 Step4 - decoding structured json

--- Earthquakes/Models/QuakeLocation.swift
+++ Earthquakes/Models/QuakeLocation.swift
@@ -1,6 +1,20 @@
 import Foundation
 
-struct QuakeLocation {
+struct QuakeLocation: Decodable {
     var latitude: Double
     var longitude: Double
+
+    struct RootProperties: Decodable {
+        var products: Products
+    }
+    struct Products: Decodable {
+        var origin: [Origin]
+    }
+    struct Origin: Decodable {
+        var properties: OriginProperties
+    }
+    struct OriginProperties {
+        var latitude: Double
+        var longitude: Double
+    }
 }
