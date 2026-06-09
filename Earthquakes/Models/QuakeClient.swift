b3a60b7c317ad94800e2c390e08dc8a841a1e6f3
soraya-ire <rujiangkongshi@gmail.com>
2026-06-09 11:15:29 +0900

    Complete Section1 Step6 - building-a-network-test-client

--- Earthquakes/Models/QuakeClient.swift
+++ Earthquakes/Models/QuakeClient.swift
@@ -9,4 +9,10 @@ struct QuakeClient {
     }()
 
     private let feedURL = URL(string: "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson")!
+
+    private let downloader: any HTTPDataDownloader
+
+    init(downloader: any HTTPDataDownloader = URLSession.shared) {
+        self.downloader = downloader
+    }
 }
