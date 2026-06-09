f5106e6cfc9c7fe20b168c969255cf0e0ed5fbc3
soraya-ire <rujiangkongshi@gmail.com>
2026-06-09 10:53:20 +0900

    Complete Section1 Step2 - building-a-network-test-client

--- Earthquakes/Models/HTTPDataDownloader.swift
+++ Earthquakes/Models/HTTPDataDownloader.swift
@@ -3,3 +3,8 @@ import Foundation
 protocol HTTPDataDownloader: Sendable {
     func httpData(from: URL) async throws -> Data
 }
+
+extension URLSession: HTTPDataDownloader {
+    func httpData(from url: URL) async throws -> Data {
+    }
+}
