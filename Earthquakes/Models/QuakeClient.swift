678e2b76f12bee4fdbfe3194ff323761eded766a
soraya-ire <rujiangkongshi@gmail.com>
2026-06-09 11:10:27 +0900

    Complete Section1 Step5 - building-a-network-test-client

--- Earthquakes/Models/QuakeClient.swift
+++ Earthquakes/Models/QuakeClient.swift
@@ -2,5 +2,11 @@ import Foundation
 
 struct QuakeClient {
 
+    private var decoder: JSONDecoder  = {
+        let aDecoder = JSONDecoder()
+        aDecoder.dateDecodingStrategy = .millisecondsSince1970
+        return aDecoder
+    }()
+
     private let feedURL = URL(string: "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson")!
 }
