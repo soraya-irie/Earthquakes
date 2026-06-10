0df0605e502555793a9ef63aeee4222666c5cfc3
soraya-ire <rujiangkongshi@gmail.com>
2026-06-10 11:29:40 +0900

    Complete Section2 Step3 - building-a-network-test-client

--- Earthquakes/Preview Content/TestDownloader.swift
+++ Earthquakes/Preview Content/TestDownloader.swift
@@ -2,6 +2,7 @@ import Foundation
 
 final class TestDownloader: HTTPDataDownloader {
     func httpData(from url: URL) async throws -> Data {
-        fatalError("Unimplemented")
+        try await Task.sleep(for: .milliseconds(.random(in: 100...500)))
+        return testQuakesData
     }
 }
